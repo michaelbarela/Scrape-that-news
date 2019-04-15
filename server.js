62ec19c24579c0dc958da3dd181092c0d3ab3129
Michael B <michaelbarela17@gmail.com>
2019-04-15 15:15:20 -0700

    Debugging

--- server.js
+++ server.js
@@ -28,10 +28,15 @@ app.set('view engine', 'handlebars');
 // Make public a static dir to serve our static files
 app.use(express.static("public"));
 
-// Mongoose (orm) connects to our mongo db and allows us to have access to the MongoDB commands for easy CRUD 
-//mongoose.connect("mongodb://heroku_f9jqr8qs:efv0pqfn8qdqhqcv7k6fr8fhg@ds161039.mlab.com:61039/heroku_f9jqr8qs");
-var db = mongoose.connection;
 
+var dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true
+});
+mongoose.set('useCreateIndex', true);
+
+var db = mongoose.connection;
 // if any errors than console errors
 db.on("error", function (error) {
     console.log("Mongoose Error: ", error);
@@ -47,5 +52,5 @@ require("./controllers/articlesController.js")(app);
 
 //Listen on PORT 8000 & notify us.
 app.listen(PORT, function () {
-    console.log("App running on port 8 THOUSAND!!!!!!!!");
+    console.log("App running on localhost:8000 !!!!!");
 });
